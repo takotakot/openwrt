cc124dd51a79753943f2f89ef2eeeb3307947bce
INAGAKI Hiroshi <musashino.open@gmail.com>
2019-03-19 13:34:06 +0900

    mediatek: mt7622: add partial support for Buffalo WSR-2533DHP2
    This commit contains following old commits:
    
    mediatek: fix initramfs image for WSR-2533DHP2
    
    mediatek: fix buffalo-initramfs-trx
    
    mediatek: fix trx magic for WSR-2533DHP2
    
    mediatek: update dts for WSR-2533DHP2
    
    - add MAC address configuration
    - add read-only flag into partition nodes
    
    mediatek: add missing dt-label for WSR-2533DHP2
    
    mediatek: update "firmware" partition for WSR-2533DHP2
    
    mediatek: add keys for WSR-2533DHP2
    
    mediatek: add LEDs for WSR-2533DHP2
    
    mediatek: add led-pins pinctrl node for WSR-2533DHP2
    
    mediatek: fix ethernet MAC address configurations for WSR-2533DHP2
    
    mediatek: drop unused pci1 node for WSR-2533DHP2
    
    mediatek: add MT7615 wireless support for WSR-2533DHP2
    
    mediatek: drop magic for trx for WSR-2533DHP2
    
    mediatek: add missing dt-label for WSR-2533DHP2
    
    mediatek: use dt-label for cpu nodes for WSR-2533DHP2
    
    mediatek: fix serial settings in chosen node for WSR-2533DHP2
    
    mediatek: fix pcie0 reg value for WSR-2533DHP2
    
    mediatek: add console parameter into bootargs for WSR-2533DHP2
    
    mediatek: mt7622: add dts file for WSR-2533DHP2 into files-4.19
    
    mediatek: mt7622: fix mtd-mac-address offset in gmac0 for WSR-2533DHP2
    
    mediatek: mt7622: drop unused dt-label for WSR-2533DHP2
    
    mediatek: mt7622: drop wpad-basic from DEVICE_PACKAGES
    
    already added in DEFAULT_PACKAGES in mediatek/Makefile
    
    mediatek: mt7622: split DEVICE_TITLE for WSR-2533DHP2
    
    mediatek: mt7622: change device name schema for WSR-2533DHP2
    
    mediatek: mt7622: remove some changes in Kernel 4.14 for WSR-2533DHP2
    
    - mt7622-buffalo-wsr-2533dhp2.dts
    - 0065-add-dtb-for-buffalo-wsr-2533dhp2.patch
    
    mediatek: mt7622: re-configure ethernet for WSR-2533DHP2
    
    mediatek: mt7622: fix fixed-link speed for WSR-2533DHP2
    
    [    0.998807] libphy: Fixed MDIO Bus: probed
    [    1.003476] sean debug physpeed = 2500
    [    1.030058] libphy: mdio: probed
    [    1.033619] swphy: unknown speed
    [    1.036947] mtk_soc_eth 1b100000.ethernet: error bringing up device

--- target/linux/mediatek/image/mt7622.mk
+++ target/linux/mediatek/image/mt7622.mk
@@ -1,3 +1,23 @@
+define Build/buffalo-initramfs-trx
+	$(STAGING_DIR_HOST)/bin/trx $(1) \
+		-o $@.new -f $@ -a 4
+  mv $@.new $@
+endef
+
+define Device/buffalo_wsr-2533dhp2
+  DEVICE_VENDOR := Buffalo
+  DEVICE_MODEL := WSR-2533DHP2
+  DEVICE_DTS := mt7622-buffalo-wsr-2533dhp2
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  BLOCK_SIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_INITRAMFS = kernel-bin | lzma | \
+    fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb | \
+    buffalo-initramfs-trx
+  DEVICE_PACKAGES := kmod-mt7615e
+endef
+TARGET_DEVICES += buffalo_wsr-2533dhp2
+
 define Device/mediatek_mt7622-rfb1
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := MTK7622 rfb1 AP
