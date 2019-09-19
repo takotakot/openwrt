8bd863ba80fbe46748eed098948609b9ae2511b0
INAGAKI Hiroshi <musashino.open@gmail.com>
2019-09-19 19:46:17 +0900

    firmware-utils: elx-header: update (WORKING)

--- tools/firmware-utils/src/elx-header.c
+++ tools/firmware-utils/src/elx-header.c
@@ -6,16 +6,16 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/stat.h>
-#include <endian.h>	/* for __BYTE_ORDER */
-#include <zlib.h>	/* for crc32 */
-#include "md5.h"	/* for md5sum */
+#include <endian.h>			/* for __BYTE_ORDER */
+#include <zlib.h>			/* for crc32 */
+#include "md5.h"			/* for md5sum */
 
-static int image_type = 3;
+static int image_type = 0xFF;	/* default: NONE */
 static int hw_id = 0;
 static size_t data_len = 0;
 static int is_hex_pattern;
 
-struct kernel {
+struct header {
 	uint32_t space1;
 	uint32_t image_type;
 	uint8_t unknown1[34];	/* not required */
@@ -29,10 +29,12 @@ struct kernel {
 	uint32_t magic_key;		/* unknown, not required */
 } __attribute ((packed));
 
-struct kernel_elecom1701 {
+struct header_elecom1701 {
+	/* -- ELECOM header -- */
 	char id[8];
 	char product[32];		/* not required */
 	char fw_ver1[16];
+	/* ------------------- */
 	uint32_t magic;
 	uint32_t data_len;
 	uint16_t image_type;
@@ -49,10 +51,26 @@ struct kernel_elecom1701 {
 	uint32_t header_crc;	/* unknown, not required */
 } __attribute ((packed));
 
-#define ELECOM1701_ID	"ELECOM"
+typedef struct {
+	uint8_t id;
+	char *name;
+} firmware_type;
+
+static firmware_type FIRMWARE_TYPES[] = {
+	{ 0x01, "bootloader" },
+	{ 0x03, "kernel" },
+	{ 0x05, "apps" },
+	{ 0x06, "kernel_elecom1701" },
+	{ 0x07, "userconfig" },
+	{ 0x0b, "factoryapps" },
+	{ 0x0d, "langpack" },
+	{ 0x0f, "cust_logo" },
+};
+
+#define ELECOM1701_ID		"ELECOM"
 #define ELECOM1701_MAGIC	0x031d6129
-#define DEF_COMP_TYPE	0x0
-#define DEF_FW_VER		"0.00"
+#define DEF_COMP_TYPE		0x0
+#define DEF_FW_VER			"0.00"
 
 #if (__BYTE_ORDER == __LITTLE_ENDIAN)
 #	define HOST_TO_LE16(x)	(x)
@@ -75,12 +93,12 @@ xor_data(uint8_t *data, size_t len, const char *pattern, int p_len, int p_off)
 	}
 }
 
-static void add_kernel_hdr(unsigned char *buf, size_t data_len)
+static void add_hdr(unsigned char *buf, size_t data_len)
 {
 	MD5_CTX ctx;
-	struct kernel header = {
+	struct header header = {
 		0,
-		HOST_TO_BE32(3),
+		HOST_TO_BE32(image_type),
 		"",
 		HOST_TO_BE32(hw_id),
 		"",
@@ -88,16 +106,16 @@ static void add_kernel_hdr(unsigned char *buf, size_t data_len)
 	};
 
 	MD5_Init(&ctx);
-	MD5_Update(&ctx, buf + sizeof(struct kernel), data_len);
+	MD5_Update(&ctx, buf + sizeof(struct header), data_len);
 	MD5_Final(header.md5sum, &ctx);
 
-	memcpy(buf, &header, sizeof(struct kernel));
+	memcpy(buf, &header, sizeof(struct header));
 }
 
 static void add_elecom1701_hdr(unsigned char *buf, size_t data_len)
 {
 	unsigned long crc;
-	struct kernel_elecom1701 header = {
+	struct header_elecom1701 header = {
 		ELECOM1701_ID,
 		"",
 		DEF_FW_VER,
@@ -113,21 +131,21 @@ static void add_elecom1701_hdr(unsigned char *buf, size_t data_len)
 
 	printf("timestamp: %x\n", header.timestamp);
 
-	crc = crc32(0, buf + sizeof(struct kernel_elecom1701), data_len);
+	crc = crc32(0, buf + sizeof(struct header_elecom1701), data_len);
 	printf("crc32: %lx\n", crc);
 	header.data_crc = HOST_TO_BE32(crc);
 
-	memcpy(buf, &header, sizeof(struct kernel_elecom1701));
+	memcpy(buf, &header, sizeof(struct header_elecom1701));
 }
 
 int main(int argc, char **argv)
 {
-	int c, tmp, p_len;
+	int c, i, tmp, p_len;
 	unsigned int hex_buf;
 	char *e, *ifn = NULL, *ofn = NULL, *pattern = NULL;
 	char hex_pattern[128];
 	unsigned char *buf;
-	size_t hdr_len; 
+	size_t hdr_len;
 	struct stat st;
 	FILE *in, *out;
 
@@ -144,9 +162,15 @@ int main(int argc, char **argv)
 			break;
 		case 't':
 			tmp = strtol(optarg, NULL, 10);
-			if (tmp == 3 || tmp == 6) {
-				image_type = tmp;
-			} else {
+			for (i = 0; i < sizeof(FIRMWARE_TYPES) / sizeof(firmware_type);
+					i++) {
+				if (FIRMWARE_TYPES[i].id == tmp) {
+					image_type = FIRMWARE_TYPES[i].id;
+					printf("image type: %s (%d)\n",
+						FIRMWARE_TYPES[i].name, FIRMWARE_TYPES[i].id);
+				}
+			}
+			if (image_type == 0xFF) { /* if unmatched */
 				fprintf(stderr, "invalid image type\n");
 				return 1;
 			}
@@ -185,17 +209,15 @@ int main(int argc, char **argv)
 	}
 
 	data_len = st.st_size;
-//	printf("data_len: 0x%lx\n", data_len);
-	if (image_type == 3)
-		hdr_len = sizeof(struct kernel);
-	else if (image_type == 6)
-		hdr_len = sizeof(struct kernel_elecom1701);
-//	printf("hdr_len: 0x%lx\n", hdr_len);
+	printf("data_len: 0x%lx\n", data_len);
+	if (image_type != 6)
+		hdr_len = sizeof(struct header);
+	else
+		hdr_len = sizeof(struct header_elecom1701);
+	printf("hdr_len: 0x%lx\n", hdr_len);
 	buf = malloc(hdr_len + data_len);
 
-	size_t read =
-		fread(buf + hdr_len, 1, data_len, in);
-//	printf("read: %lx\n", read);
+	size_t read = fread(buf + hdr_len, 1, data_len, in);
 	if (read != data_len){
 		fprintf(stderr, "read err\n");
 		return 1;
@@ -234,22 +256,16 @@ int main(int argc, char **argv)
 		printf("pattern: %s\n", pattern);
 	}
 
-	if (image_type == 3) {
-		add_kernel_hdr(buf, data_len);
+	if (image_type != 6)
+		add_hdr(buf, data_len);
 
-		if (is_hex_pattern)
-			xor_data(buf + hdr_len, data_len, hex_pattern, (p_len / 2), 0);
-		else
-			xor_data(buf + hdr_len, data_len, pattern, p_len, 0);
+	if (is_hex_pattern)
+		xor_data(buf + hdr_len, data_len, hex_pattern, (p_len / 2), 0);
+	else
+		xor_data(buf + hdr_len, data_len, pattern, p_len, 0);
 
-	} else if (image_type == 6) {
-		if (is_hex_pattern)
-			xor_data(buf + hdr_len, data_len, hex_pattern, (p_len / 2), 0);
-		else
-			xor_data(buf + hdr_len, data_len, pattern, p_len, 0);
-
+	if (image_type == 6)
 		add_elecom1701_hdr(buf, data_len);
-	}
 
 	fwrite(buf, 1, hdr_len + data_len, out);
 
