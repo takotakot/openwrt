4a881d6861e3d16ac88551d2ee3909c5d8a783f1
INAGAKI Hiroshi <musashino.open@gmail.com>
2020-03-12 02:32:46 +0900

    ipq807x: add preliminary support for Buffalo WXR-5950AX12

--- target/linux/ipq807x/image/arm.mk
+++ target/linux/ipq807x/image/arm.mk
@@ -1,1 +1,11 @@
-# no entry
+define Device/buffalo_wxr-5950ax12
+	$(call Device/FitImage)
+	SOC := ipq8078
+	DEVICE_VENDOR := Buffalo
+	DEVICE_MODEL := WXR-5950AX12
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	KERNEL_IN_UBI := 1
+	IMAGES := sysupgrade.bin
+endef
+TARGET_DEVICES += buffalo_wxr-5950ax12
