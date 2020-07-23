e706a350b2cf66a4ec81f2e04bcf431c459efe93
INAGAKI Hiroshi <musashino.open@gmail.com>
2020-07-23 23:14:49 +0900

    ath79: add support for ELECOM WAB-I1750-PS

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -761,6 +761,15 @@ define Device/dlink_dir-859-a1
 endef
 TARGET_DEVICES += dlink_dir-859-a1
 
+define Device/elecom_wab-i1750-ps
+  SOC := qca9558
+  DEVICE_VENDOR := ELECOM
+  DEVICE_MODEL := WAB-I1750-PS
+  IMAGE_SIZE := 14336k
+  DEVICE_PACKAGES := kmod-ath10k-ct ath10k-firmware-qca988x-ct
+endef
+TARGET_DEVICES += elecom_wab-i1750-ps
+
 define Device/elecom_wrc-1750ghbk2-i
   SOC := qca9563
   DEVICE_VENDOR := ELECOM
